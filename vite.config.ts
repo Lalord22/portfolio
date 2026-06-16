91ffe1d6c686314e9918d33de245cc50db38529b
Lalord22 <lalo22296@hotmail.com>
2026-06-16 00:17:53 -0600

    Revert "udpate to favicon and config"
    This reverts commit dc9743f39a6948fdcae7f27a9f3440a3ee987333.

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => {
-  const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
-  const githubPagesBase = repositoryName ? `/${repositoryName}/` : './'
-  const productionBase = process.env.VITE_BASE_PATH ?? githubPagesBase
+  const productionBase = process.env.VITE_BASE_PATH ?? '/portfolio/'
 
   return {
     plugins: [react()],
